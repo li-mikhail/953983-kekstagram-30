cd75bfa84dd087913a96c293e53129422aa431ed
Mikhail Li <leemisha98@gmail.com>
2023-10-17 00:07:52 +0300

    Updated functions

--- js/functions.js
+++ js/functions.js
@@ -3,35 +3,58 @@ function checkLength(string, numberOfSymbols) {
   return string.length <= numberOfSymbols;
 }
 
-function isPalyndrome(string) {
-  const prepWord = string.toLowerCase().replaceAll(' ', '');
-  let start = 0;
-  let end = prepWord.length - 1;
+// function isPalyndrome(string) {
+//   const prepWord = string.toLowerCase().replaceAll(' ', '');
+//   let start = 0;
+//   let end = prepWord.length - 1;
 
-  while (start < end) {
-    if (prepWord[start] !== prepWord[end]) {
+//   while (start < end) {
+//     if (prepWord[start] !== prepWord[end]) {
+//       return false;
+//     }
+//     start++;
+//     end--;
+//   }
+//   return true;
+// }
+
+function isPalyndrome(rawString) {
+  const string = rawString.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string.at(i) !== string.at(-i - 1)) {
       return false;
     }
-    start++;
-    end--;
   }
   return true;
 }
 
-function toNumber(string) {
-  if (typeof string !== 'string') {
-    string = string.toString();
-  }
+// function toNumber(string) {
+//   if (typeof string !== 'string') {
+//     string = string.toString();
+//   }
+
+//   let resultSrting = '';
+//   for (let i = 0; i <= string.length - 1; i++) {
+//     const symbol = parseInt(string[i], 10);
+
+//     if(!Number.isNaN(symbol) && symbol !== ' ') {
+//       resultSrting += symbol.toString();
+//     }
+//   }
+//   return resultSrting ? resultSrting : NaN;
+// }
+
+function toNumber(arg) {
+  const string = arg.toString();
 
   let resultSrting = '';
-  for (let i = 0; i <= string.length - 1; i++) {
-    const symbol = parseInt(string[i], 10);
+  for (let i = 0; i < string.length; i++) {
 
-    if(!Number.isNaN(symbol) && symbol !== ' ') {
-      resultSrting += symbol.toString();
+    if(!Number.isNaN(parseInt(string[i], 10))) {
+      resultSrting += string[i];
     }
   }
-  return resultSrting ? resultSrting : NaN;
+  return parseInt(resultSrting, 10);
 }
 
-//console.log(toNumber(22));
+//console.log(toNumber('22 vfwevwebv 33'));
