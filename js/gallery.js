5a2bd06d383558ba08d3fea412b5f6195abe0c35
Mikhail Li <leemisha98@gmail.com>
2023-11-24 15:11:26 +0200

    Fixed multiple event listeners addition to rendered images

--- js/gallery.js
+++ js/gallery.js
@@ -3,20 +3,24 @@ import { showPicture } from './bigPicture.js';
 
 const container = document.querySelector('.pictures');
 
+let firstRender = true;
+
 const renderGallery = (pictures) => {
-  container.addEventListener('click', (evt) => {
-    const thumbnail = evt.target.closest('[data-thumbnail-id]'); //finds closet parent element by selector or in our case the element which has this attribute, if element not found we will get null
-    if (! thumbnail) {
-      return;
-    }
+  if(firstRender) {
+    container.addEventListener('click', (evt) => {
+      const thumbnail = evt.target.closest('[data-thumbnail-id]'); //finds closet parent element by selector or in our case the element which has this attribute, if element not found we will get null
+      if (! thumbnail) {
+        return;
+      }
+      evt.preventDefault();
 
-    evt.preventDefault();
+      const thumbnailId = +thumbnail.dataset.thumbnailId; //the data in dataset is a stroke, so we ahve to conver it to number
+      const pictureData = pictures.find(({ id }) => id === thumbnailId); //find specific object which has the same attribute as a thumbnail user clicked
 
-    const thumbnailId = +thumbnail.dataset.thumbnailId; //the data in dataset is a stroke, so we ahve to conver it to number
-    const pictureData = pictures.find(({ id }) => id === thumbnailId); //find specific object which has the same attribute as a thumbnail user clicked
-
-    showPicture(pictureData);
-  });
+      showPicture(pictureData);
+    });
+    firstRender = false;
+  }
 
   renderThumbnails(pictures, container);
 };
